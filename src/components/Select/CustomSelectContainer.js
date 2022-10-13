fdfc470e30c0ba03d7d86b6b3b3a47a245afd6d6
Chaithra <chaithrapai@gmail.com>
2022-10-13 19:04:25 +0530

    Added Dropdown component

--- src/components/Select/CustomSelectContainer.js
+++ src/components/Select/CustomSelectContainer.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import SelectCustom from './SelectCustom'
 import colourOptions from '../../sampleData.js'
+import './CustomSelectContainer.css'
 
 const CustomSelectContainer = () => {
   return (
-    <div>
-        <SelectCustom options={colourOptions}/>
-        
+    <div className='filterButtonDesktop'>
+        <SelectCustom options={colourOptions} label='Type'/>  
+        <SelectCustom options={colourOptions} label='Gender'/> 
+        <SelectCustom options={colourOptions} label='Stat'/> 
     </div>
   )
 }
--- src/components/Select/SelectCustom.js
+++ src/components/Select/SelectCustom.js
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ReactComponent as ArrowIcon  } from '../../assets/icons/downArrow_icon.svg'
 import './SelectCustom.css'
-import colourOptions from '../../sampleData.js'
-
 
 const SelectCustom = (props) => {
+    const options = props.options
+    const [expanded,setExpanded] = useState(false);
 
+    const customStyle = {
+        backgroundColor: expanded ? '#fff' : '#cddce1'
+    }
 
     return (
-        <div className='filterDesktop'>
-            <div className='selectLabel'><p>Type</p></div>
-            <div className='selectDropdown'>
-                <div className='dropdownLabel'>
-                    <input type='checkbox' className='customInput' checked={true} />
+        <div className='selectContainer'>
+            <div className='selectLabel'><p>{props.label}</p></div>
+            <div style={customStyle} className='selectDropdownContainer'
+            >
+                <div className='customSelectLabel' style={customStyle}>
+                    <div className='containerLabel' style={customStyle}>Normal</div>
+                    <ArrowIcon className='icon'style={customStyle} /> 
                 </div>
-                <div className='downArrowIcon'>
-                    <ArrowIcon className='icon'></ArrowIcon>  
-                </div>        
             </div>
+
+            {   expanded && (
+                    <div className='dropdownOptionsContainer'>
+                        {
+                            options.map((item,index) => (
+                                <>
+                                <div className='filterLists'>
+                                    <input
+                                        type='checkbox'
+                                        id={item.value}
+                                        className='filterCheckbox'
+                                    />
+                                    <label htmlFor={item.value} className='filterLabel'>
+                                        {item.label.toUpperCase()}
+                                    </label>
+                                </div>
+                                <hr className='customHorizontalLine'/>
+                                </>
+                            ))  
+                        }
+                        
+                </div>
+                )
+            }
         </div>
     )
 }
