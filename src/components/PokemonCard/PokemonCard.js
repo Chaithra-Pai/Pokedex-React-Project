c69a4eb04eb207ff070aa0113956e5e8bcbedb1e
Chaithra <chaithrapai@gmail.com>
2022-10-14 17:42:52 +0530

    Updating

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -1,28 +1,38 @@
 import './pokemonCard.css'
-import React from 'react'
+import React, { useState } from 'react'
 import Squirtle from './squirtle.png'
 import { useSelector } from 'react-redux'
 import { LoadingIndicators } from '../../constants/constants'
 import { getPokemonListObject } from '../../redux/selector/pokemonListSelector'
-
+import axios from 'axios';
 
 const PokemonCard = () => {
 
   const { loading, pokemonListResponse, pokemonListError } = useSelector(getPokemonListObject)
   const pokemonListResult = pokemonListResponse.results;
-  console.log(pokemonListResponse.results);
-  
+  // console.log(pokemonListResponse.results);
+
   return (
     <div className='pokemonList'>
       { 
-        pokemonListResult.map((pokemon,index) => (
-          <div className='pokemonCard'>
-              <img className='pokemonImg' alt='Pokemon' src={Squirtle}>
-              </img>
+         pokemonListResult?.map((pokemon,index) => {
+          let url = pokemon.url
+          let id = axios
+          .get(url)
+          .then((response) => {
+              let pokemonId = response.data.id
+              return pokemonId
+          })
+          .catch((error) => console.log(error))
+          
+          return (  
+          <div className='pokemonCard' key={index}>
+              <img className='pokemonImg' alt='Pokemon' src={Squirtle} />
               <p className='pokemonLabel'>{ pokemon.name }</p>
               <p className='pokemonId'>001</p>
           </div>
-        ))
+         )
+        })
       }
     </div> 
   )
